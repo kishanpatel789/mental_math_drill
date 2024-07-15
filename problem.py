88a97a3be0f4d8416bd71dc506cf7c5ab8ca7103
Kishan Patel <mail@kishanpatel.dev>
2024-07-15 16:23:23 -0500

    Create problem generator

--- problem.py
+++ problem.py
@@ -1,4 +1,8 @@
+import random
+
 class Problem():
+    valid_operators = ['+', '-', '*', '/']
+
     def __init__(self, first_num, second_num, operator) -> None:
         if isinstance(first_num, int) and isinstance(second_num, int):
             self.first_num = first_num
@@ -6,10 +10,10 @@ class Problem():
         else:
             raise TypeError(f"First number and second number should be integers.")
         
-        if operator in ('+', '-', '*', '/'):
+        if operator in self.__class__.valid_operators:
             self.operator = operator
         else:
-            raise ValueError(f"Operator should be one of '+', '-', '*', '/'. Recieved '{operator}'.")
+            raise ValueError(f"Operator should be one of {self.__class__.valid_operators}. Recieved '{operator}'.")
         
         if operator == '+':
             self.answer = first_num + second_num
@@ -31,3 +35,15 @@ class Problem():
         output += f"{'‾'*(max_num_len + 3)}"
 
         return output
+
+def generate_problems(num_probs: int):
+    problems = []
+
+    for _ in range(num_probs):
+        operator = random.choice(Problem.valid_operators)
+        first_num = random.randint(2, 999)
+        second_num = random.randint(2, 999)
+        prob = Problem(first_num, second_num, operator)
+        problems.append(prob)
+
+    return problems
