9592980182ef197749e9518f84e85297f9198b87
Kishan Patel <mail@kishanpatel.dev>
2024-07-22 15:37:26 -0500

    Move main.py to execution routine

--- main.py
+++ main.py
@@ -5,42 +5,46 @@ from datetime import datetime
 from problem import generate_problems
 from jinja2 import Environment, FileSystemLoader
 
-# generate problems and html
-problems = generate_problems(5, ['+', '-'])
-env = Environment(
-    loader=FileSystemLoader('./templates'),
-    trim_blocks=True,
-    lstrip_blocks=True,
-)
-template_txt = env.get_template('template.txt')
-template_html = env.get_template('template.html')
+def main():
+    # generate problems and html
+    problems = generate_problems(5, ['+', '-'])
+    env = Environment(
+        loader=FileSystemLoader('./templates'),
+        trim_blocks=True,
+        lstrip_blocks=True,
+    )
+    template_txt = env.get_template('template.txt')
+    template_html = env.get_template('template.html')
 
-output_txt = template_txt.render(problems=problems)
-output_html = template_html.render(problems=problems)
-with open('out/out.txt', 'w') as f:
-    f.write(output_txt)
-with open('out/out.html', 'w') as f:
-    f.write(output_html)
+    output_txt = template_txt.render(problems=problems)
+    output_html = template_html.render(problems=problems)
+    # with open('out/out.txt', 'w') as f:
+    #     f.write(output_txt)
+    # with open('out/out.html', 'w') as f:
+    #     f.write(output_html)
 
-# generate email
-sender = 'Mental Math Drill <no-reply@kpdata.dev>'
-recipient = 'Kishan <kishanpatel789@gmail.com>'
-subject = f"Mental Math Drill - {datetime.today().strftime('%m/%d')}"
+    # generate email
+    sender = 'Mental Math Drill <no-reply@kpdata.dev>'
+    recipient = 'Kishan <kishanpatel789@gmail.com>'
+    subject = f"Mental Math Drill - {datetime.today().strftime('%m/%d')}"
 
-message = MIMEMultipart('alternative')
-message['From'] = sender
-message['To'] = recipient
-message['Subject'] = subject
+    message = MIMEMultipart('alternative')
+    message['From'] = sender
+    message['To'] = recipient
+    message['Subject'] = subject
 
-message.attach(MIMEText(output_txt, 'plain'))
-message.attach(MIMEText(output_html, 'html'))
+    message.attach(MIMEText(output_txt, 'plain'))
+    message.attach(MIMEText(output_html, 'html'))
 
-# send email
-ses = boto3.client('sesv2')
-ses.send_email(
-    Content={
-        'Raw': {
-            'Data': message.as_bytes()
+    # send email
+    ses = boto3.client('sesv2')
+    ses.send_email(
+        Content={
+            'Raw': {
+                'Data': message.as_bytes()
+            }
         }
-    }
-)
+    )
+
+if __name__ == '__main__':
+    main()
